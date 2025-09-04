f112b64f51a593af85a56d47f4fba7e42139395e
yigonghu <gongxini@uw.edu>
2025-09-04 17:24:55 -0400

    Fix bugs

--- src/misc/toolchain-build.sh
+++ src/misc/toolchain-build.sh
@@ -148,7 +148,7 @@ if [ $tool == "all" -o $tool == "binutils" ]; then
   download_and_check https://ftp.gnu.org/gnu/binutils/binutils-2.43.tar.bz2 fed3c3077f0df7a4a1aa47b080b8c53277593ccbb4e5e78b73ffb4e3f265e750
 fi
 if [ $tool == "all" -o $tool == "gcc" ]; then
-  download_and_check https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2 9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5
+  download_and_check https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2
   if [ ! -d $CWD/src/gcc-6.2.0/mpfr ]; then
     cd $CWD/src/gcc-6.2.0 && contrib/download_prerequisites || perror "Failed to download pre-requisite for GCC"
   fi
